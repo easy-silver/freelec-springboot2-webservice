6261b262de895f0d80268d5c783832eea852e847
Timo <developer.timo@gmail.com>
2020-07-29 12:42:19 +0900

    글 삭제 기능 추가

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -7,7 +7,11 @@ var main = {
         $('#btn-update').on('click', function() {
             _this.update();
         });
+        $('#btn-delete').on('click', function() {
+            _this.delete();
+        });
     },
+
     save : function() {
         var data = {
             title : $('#title').val(),
@@ -28,6 +32,7 @@ var main = {
             alert(JSON.stringify(error));
         });
     },
+
     update : function() {
         var data = {
             title: $('#title').val(),
@@ -44,6 +49,21 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function(error) {
+            alert(JSON.stringify(error));
+        });
+    },
+    delete : function() {
+        var id = $('#id').val();
+
+        $.ajax({
+            type : 'DELETE',
+            url : '/api/v1/posts/' + id,
+            dataType : 'JSON',
+            contentType : 'application/json; charset=UTF-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function(error) {
             alert(JSON.stringify(error));
